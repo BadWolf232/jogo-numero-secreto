d85e12555d0a65f47016e748e651a19985cb0f61
Vitor Souza <116305504+BadWolf232@users.noreply.github.com>
2024-10-16 15:53:11 -0300

    Add files via upload

---
+++ js-funções-listas/exercicios.js
@@ -0,0 +1,123 @@
+// function ativaAnuncio (texto){
+//     console.log(texto);
+// }
+
+// ativaAnuncio('Olá Mundo!');
+
+//////////////////////////////////
+
+// let nomeU = prompt("Qual é seu nome? ")
+
+// nomeUsuario(nomeU)
+
+// function nomeUsuario(nome){
+//     console.log(`Olá, ${nome} !A`);
+// }
+
+//////////////////////////////////
+
+// let num = prompt("Digite um numero: ");
+
+// dobroNum(num);
+
+// function dobroNum(nume){
+//     let dobro = nume * 2;
+//     console.log(`O numero dobrado é ${dobro}`);
+// }
+
+/////////////////////////////////
+
+// let numA = parseInt(prompt("Digite um numero A: "));
+// let numB = parseInt(prompt("Digite um numero B: "));
+// let numC = parseInt(prompt("Digite um numero C: "));
+
+// let soma  = numA + numB + numC;
+
+// console.log(soma);
+
+// media(soma);
+
+// function media(num) {
+//     let media = num/3; 
+//     console.log(`A média é ${media} !`);
+// }
+
+////////////////////////////////////
+
+// let numA = parseInt(prompt("Digite um numero A: "));
+// let numB = parseInt(prompt("Digite um numero B: "));
+
+// maior(numA,numB)
+
+// function maior(num,mun){
+//     if (num > mun){
+//         return console.log(`O maior é ${num}`);
+//     }else{
+//         return console.log(`O maior é ${mun}`)
+//     }
+// }
+
+///////////////////////////////////// 
+
+// let num = parseInt(prompt("Insira um valor: "));
+
+// multi(num)
+
+// function multi (mun){
+//     let valor = mun * mun; 
+//     console.log(`O valor é ${valor} !`);
+// }
+
+
+// let peso = parseInt(prompt('Qual é seu peso?'));
+// let altura = parseFloat(prompt('Qual é sua altura?'))
+
+// function imc(peso, altura) {
+//     let alturas = altura * altura; 
+//     let icm = peso / alturas;
+//     let iccm = icm.toFixed(1);
+//     console.log(`Seu índice de massa corporal é ${iccm} Kg/m2`);
+// }
+// imc(peso,altura);
+////////////////////////////////////////
+
+// let valor = parseInt(prompt("Insira um valor: "));
+// fator(valor2)
+
+// function fator(valor) {
+//     if (valor == 0 || valor == 1){
+//         return 1;
+//     }else{
+//         return valor *fator(valor - 1);
+//     }
+// }
+
+// console.log(`O fatorial de ${valor} é ${fator(valor)}`);
+
+///////////////////////////////////////
+
+//let listaGenerica = []; 
+
+//////////////////////////////////////
+
+// let linguagemDeProgramacao = ['JavaScript','C','C++','Kotlin','Python'];
+
+// console.log(linguagemDeProgramacao);
+
+// let programasNovos1 = 'Java';
+// let programasNovos2 = 'Ruby';
+// let programasNovos3 = 'GoLang';
+
+// linguagemDeProgramacao.push(programasNovos1,programasNovos2,programasNovos3)
+
+// console.log(linguagemDeProgramacao)
+
+///////////////////////////////////////
+
+// let nomesLindos = ['Cailee','Angorie','Olivia'];
+
+// console.log(`O primeiro nome é ${nomesLindos[0]}`);
+// console.log(`O segundo nome é ${nomesLindos[1]}`);
+// console.log(`O terceiro nome é ${nomesLindos[2]}`);
+
+
